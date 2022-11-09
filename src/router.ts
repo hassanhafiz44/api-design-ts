2e60d5f31a576af80cfbd4ecd7d9e8d2a281cb4f
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2022-11-10 01:11:10 +0500

    abstracted the repeating code into middleware

--- src/router.ts
+++ src/router.ts
@@ -1,5 +1,6 @@
 import { Request, Response, Router } from "express";
 import { body, validationResult } from "express-validator";
+import { handleInputErrors } from "./modules/middlewares";
 
 const router = Router();
 
@@ -16,11 +17,8 @@ router.get("/product/:id", (req: Request, res: Response) => {
 router.put(
   "/product/:id",
   body("name").isString(),
+  handleInputErrors,
   (req: Request, res: Response) => {
-    const errors = validationResult(req);
-    if (!errors.isEmpty()) {
-      return res.status(400).json({ errors: errors.array() });
-    }
     const { id } = req.params;
     res.json({ message: `product with ${id}` });
   }
