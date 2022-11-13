1683d593d4f456224f4583f4095990380761a799
Muhammad Hassan Javed <hassanhafiz44@gmail.com>
2022-11-13 22:09:42 +0500

    Added error handler

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,14 @@
-import express, { NextFunction, Request, Response } from "express";
+import express, {
+  ErrorRequestHandler,
+  NextFunction,
+  Request,
+  Response,
+} from "express";
 import morgan from "morgan";
 import cors from "cors";
 import router from "./router";
 import { protect } from "./modules/auth";
 import { createNewUser, signin } from "./handlers/user";
-
 
 const app = express();
 
@@ -19,14 +23,26 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(customLogger("Hello"));
 
-app.get("/", (_req: Request, res: Response) => {
-  console.log("hello from express");
-  res.status(200);
-  res.json({ message: "hello" });
+app.get("/", (_req: Request, res: Response, next: NextFunction) => {
+  setTimeout(() => {
+    next(new Error("Hello"));
+  }, 1);
 });
 
 app.use("/api", protect, router);
 app.post("/user", createNewUser);
 app.post("/signin", signin);
+
+app.use(
+  (
+    err: ErrorRequestHandler,
+    req: Request,
+    res: Response,
+    next: NextFunction
+  ) => {
+    console.error(err);
+    res.json({ message: "There was an error" });
+  }
+);
 
 export default app;
